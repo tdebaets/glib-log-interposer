b1d56bf2b1e002c4b99189a71f9fac083cc7e719
Tim De Baets <982167+tdebaets@users.noreply.github.com>
2026-06-11 18:07:41 +0200

    post-build.sh: copy to different subdirectory depending on 32/64-bit shared library

--- Scripts/post-build.sh
+++ Scripts/post-build.sh
@@ -28,14 +28,39 @@ fi
 
 return_code=
 so_filename=$1
+so_filetype=
 deploy_dir=$GLIB_LOG_INTERPOSER_DEPLOY_DIR
-target_filename="${deploy_dir}/${so_filename}"
+arch_subdir_32="lib/i386-linux-gnu"
+arch_subdir_64="lib/x86_64-linux-gnu"
+arch_subdir=
+target_dir=
+target_filename=
 
 echo "${so_filename}"
 
 pwd
 
 if [ ! -z "${deploy_dir}" ]; then
+
+    # `file` always prints to stdout, so no point in redirecting stderr
+    so_filetype=$(file "$so_filename")
+    return_code=$?
+    
+    if [ $return_code -ne 0 ]; then
+        echo "Failed to retrieve file type for ${so_filename}"
+        exit $return_code
+    fi
+    
+    if [[ "${so_filetype}" =~ "32-bit" ]]; then
+        arch_subdir=${arch_subdir_32}
+    elif [[ "${so_filetype}" =~ "64-bit" ]]; then
+        arch_subdir=${arch_subdir_64}
+    else
+        echo "Failed to determine architecture of ${so_filename} - ${so_filetype}"
+        exit 1
+    fi
+    
+    target_filename="${deploy_dir}/${arch_subdir}/${so_filename}"
 
     if [ -f "${target_filename}" ]; then
         rm "${target_filename}"
@@ -46,7 +71,7 @@ if [ ! -z "${deploy_dir}" ]; then
         fi
     fi
 
-    cp "${so_filename}" "${deploy_dir}"
+    cp "${so_filename}" "${target_filename}"
     return_code=$?
 
     if [ $return_code -ne 0 ]; then
